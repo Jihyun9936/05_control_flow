17ff05b447e930a8dac5fdef65535933e988a8f1
Jihyun9936 <55350908+Jihyun9936@users.noreply.github.com>
2019-10-01 15:39:44 +0900

    Lab02

--- main.c
+++ main.c
@@ -4,7 +4,7 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 
-/*Lab01*/
+/*Lab01
 int main(int argc, char *argv[]) {
 	int input;
 	
@@ -15,4 +15,17 @@ int main(int argc, char *argv[]) {
 	else 
 		printf("It's not a positive number.");
 	return 0;
+}*/
+
+/*Lab02*/
+int main(){
+	int input, ab_val;
+	
+	printf("input the number : ");
+	scanf("%d", &input);
+	if(input<0)
+		ab_val=-input;
+	else
+		ab_val=input;
+	printf("The absolute value is %d", ab_val);
 }
